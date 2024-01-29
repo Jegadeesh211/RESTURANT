15b222e94392cbe772bafb9e3f156dfb2a8dcf2e
JEGADEESH R <144311950+Jegadeesh211@users.noreply.github.com>
2024-01-29 00:12:29 -0800

    Add files via upload

---
+++ food.js
@@ -0,0 +1,21 @@
+
+var bur=document.getElementById("h1");
+setTimeout((bur)=>{
+    // function myFun(){
+    //     bur
+    // }
+    // myFun();
+
+},2000);
+
+
+// setTimeout(()=>{
+//     // function myFun(){
+//     //     bur
+//     // }
+//     // myFun();
+
+// },2000);
+
+setTimeout(function(){bur }, 2000)
+
